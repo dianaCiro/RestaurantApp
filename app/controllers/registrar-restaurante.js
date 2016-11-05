867db26dfe8297e7ee12968fcc544c072a0b2d48
00Fede <feedkiko@gmail.com>
2016-11-05 13:16:09 -0500

    Actualizacion campos registro restaurante

--- app/controllers/registrar-restaurante.js
+++ app/controllers/registrar-restaurante.js
@@ -5,14 +5,11 @@ export default Ember.Controller.extend({
   actions: {
     registrar(){
       let restaurant = {
-        name: this.get('name'),
-        address: this.get('address'),
-        phone: this.get('phone'),
+        id_restaurant: this.get('id_restaurant'),
+        name_restaurant: this.get('name_restaurant'),
         description: this.get('description'),
-        timetable: this.get('timetable'),
         email: this.get('email'),
-        specialty: this.get('specialty'),
-        images: this.get('images')
+        admin: this.get('admin')
       };
       console.log(restaurant);
       let restaurantModel = this.get('store').createRecord('restaurant',restaurant);
--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -2,19 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   id_restaurant: DS.attr(),
-  name: DS.attr(),
-  address: DS.attr(),
-  city: DS.attr(),
-  phone: DS.attr(),
+  name_restaurant: DS.attr(),
   description: DS.attr(),
-  latitud: DS.attr(),
-  longitude: DS.attr(),
-  open_time_week: DS.attr(),
-  close_time_week:DS.attr(),
-  open_time_weekend:DS.attr(),
-  close_time_weekend:DS.attr(),
   admin: DS.attr(),
-  timetable: DS.attr(),
-  email: DS.attr(),
-  specialty: DS.attr()
+  email: DS.attr()
 });
