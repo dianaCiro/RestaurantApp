3acaebe6bef4fa7197e28ed203c7f08ebfdb9dde
Walter <walterac91@gmail.com>
2016-11-23 21:25:05 -0500

    filtro para buscar restaurantes por nombre

--- app/adapters/restaurant.js
+++ app/adapters/restaurant.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   //namespace: 'restaurants',
-  host: 'https://restaurants-udea-soft.herokuapp.com'
+  host: 'https://restaurant-node.herokuapp.com/api'
 });
--- app/serializers/restaurant.js
+++ app/serializers/restaurant.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 //import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
+  primaryKey :'id_restaurant',
   serialize(snapshot, options) {
 	    var json = this._super(...arguments);
 
