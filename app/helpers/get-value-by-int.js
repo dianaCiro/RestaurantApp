cadda17f766db53715a95a7d2be4ef110a529c35
00Fede <feedkiko@gmail.com>
2016-11-25 17:03:07 -0500

    Helper para determinar valor deacuerdo al int, ver /helpers/get-value-by-ini.js

---
+++ app/helpers/get-value-by-int.js
@@ -0,0 +1,82 @@
+import Ember from 'ember';
+const TAG = "[Helper get-value-by-int]: ";
+
+/**role user
+0 : cliente
+1 : administrador
+
+
+state reservation
+0 : reservada
+1 : cancelada
+2 : incumplida
+3 : completada
+
+
+type order
+0 : delivery
+1 : reservation
+
+
+type payment
+0 : no pagada
+1 : pagada
+*/
+export function getValueByInt(params/*,hash*/) {
+  let model = params[0];
+  let digit = params[1];
+  console.log(TAG+"ejecutando helper...");
+  console.log(TAG+"parametro model="+model);
+  debugger
+  console.log(TAG+"parametro digit="+digit);
+  switch (model) {
+    case 'user':
+    switch (digit) {
+      case 0:
+      return 'cliente';
+      break;
+      case 1:
+      return 'administrador'
+      break;
+    }
+    break;
+    case 'reservation':
+    switch (digit) {
+      case 0:
+      return 'reservada';
+      break;
+      case 1:
+      return 'cancelada'
+      break;
+      case 2:
+      return 'incumplida';
+      break;
+      case 3:
+      return 'completada'
+      break;
+    }
+    break;
+    case 'order':
+    switch (digit) {
+      case 0:
+      return 'delivery';
+      break;
+      case 1:
+      return 'reservation'
+      break;
+    }
+    break;
+    case 'payment':
+    switch (digit) {
+      case 0:
+      return 'Pagada';
+      break;
+      case 1:
+      return 'No Pagada'
+      break;
+    }
+    break;
+  }
+}
+
+export default Ember.Helper.helper(getValueByInt);
---
+++ tests/unit/helpers/get-value-by-int-test.js
@@ -0,0 +1,12 @@
+
+import { getValueByInt } from 'busqueda-restaurante/helpers/get-value-by-int';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get value by int');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getValueByInt([42]);
+  assert.ok(result);
+});
+
