a7106a004980d4617a6d8c762f2e98b54ee5d22a
00Fede <feedkiko@gmail.com>
2016-11-26 01:27:00 -0500

    Organizando reservas, listar todos los clientes, algo de estilos

--- app/models/cliente.js
+++ app/models/cliente.js
@@ -1,12 +1,26 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-	username: DS.attr(),
 	email: DS.attr(),
-	name: DS.attr(),
-	role: DS.attr(),
 	phone: DS.attr(),
 	identification: DS.attr(),
+	username: DS.attr(),
+	name: DS.attr(),
+	role: DS.attr(),
 	password: DS.attr(),
 	passwordConfirmation: DS.attr()
 });
+
+/**
+{
+	 "email": "jlditru@gmail.com",
+	 "phone": "3103323818",
+	 "identification": "1017228117",
+	 "token": null,
+	 "balance": 0,
+	 "username": "joluditru",
+	 "role": 1,
+	 "name": "Jorge Diaz",
+	 "password": "joluditru1234",
+	 "id_user": 10
+ },*/
