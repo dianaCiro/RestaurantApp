84594fd30307eb78a78cfeda9993161e0841163b
serloap <sergio0195@hotmail.com>
2016-11-26 04:17:00 -0500

    Advanced search filter component added, search controller and adv-search route modified

--- app/controllers/busqueda.js
+++ app/controllers/busqueda.js
@@ -16,6 +16,26 @@ export default Ember.Controller.extend({
        //console.log(TAG+"store del controller="+store);
        return this.get('store').peekAll('restaurant');
      }
+   },
+   filterAdvanced(email, description){
+     console.log("El filtro por email se esta ejecutando " +  param);
+     var filtered;
+     if (email != '') {
+       var content = this.get('content');
+       console.log(TAG+"content del controller="+content);
+       filtered = this.get('content').filter((item,index,self)=>item.get('email') === email);
+       if(description != ''){
+         filtered = filtered.filter((item,index,self)=>item.get('description') === description);
+       }
+     }
+     else if(description != ''){
+       var content = this.get('content');
+       console.log(TAG+"content del controller="+content);
+       return this.get('content').filter((item,index,self)=>item.get('description') === description);
+     }
+     else{
+       return this.get('store').peekAll('restaurant');
+     }
    }
   }
 });
--- app/routes/busqueda-avanzada.js
+++ app/routes/busqueda-avanzada.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
-
+const TAG = "[Route busqueda-avanzada.js]: ";
+console.log(TAG + "entrando a route de busqueda avanzada");
 export default Ember.Route.extend({
+  model: function(){
+    return this.store.findAll('restaurant');
+  }
 });
